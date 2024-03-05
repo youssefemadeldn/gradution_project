436c971f388d13553be2be0743b34f6747b6d908
youssef emad eldin <youssefemad63.ye@gmail.com>
2024-03-05 22:17:13 +0200

    create part 1 of home page

--- lib/views/b_home_view.dart
+++ lib/views/b_home_view.dart
@@ -10,6 +10,55 @@ class HomeView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CustomAppBar(),
+      body: Stack(
+        clipBehavior: Clip.none,
+        children: [
+          const Positioned(
+            top: 210,
+            // bottom: 150,
+            right: 8,
+            child: Text(
+              'الخدمات الذكية',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 30,
+                fontWeight: FontWeight.w900,
+              ),
+            ),
+          ),
+          Image.asset(
+            'assets/images/background-gradute1.png',
+          ),
+          const Positioned(
+            // top: 0,
+            bottom: 110,
+            right: 5,
+            child: Text(
+              'الدراسات العليا',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          const Positioned(
+            top: 80,
+            // bottom: 150,
+            right: 5,
+            child: Text(
+              '''كل مايحتاجه الطالب من مدخل واحد: تسجيل المقررات، دفع المصروفات
+  منصة التعليم الإلكترونى، الجداول الدراسية، جداول الامتحانات، النتائج
+               ''',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 13,
+                fontWeight: FontWeight.w900,
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
